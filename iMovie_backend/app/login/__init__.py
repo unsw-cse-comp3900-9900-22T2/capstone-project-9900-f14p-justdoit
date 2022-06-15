1a5124b5929ffdcfae352184ad35a03ee4cb5c63
kelurLucky <Lucky@kelur.com>
2022-06-15 16:46:14 +0800

    frontend and backend code

--- iMovie_backend/app/login/__init__.py
+++ iMovie_backend/app/login/__init__.py
@@ -3,6 +3,6 @@ from . import views, movies
 
 
 def init_route(app: Flask):
-    app.add_url_rule('/app/views/login', view_func=views.login, methods=['POST'])
-    app.add_url_rule('/app/views/register', view_func=views.register, methods=['POST'])
+    app.add_url_rule('/login', view_func=views.login, methods=['POST'])
+    app.add_url_rule('/register', view_func=views.register, methods=['POST'])
     app.add_url_rule('/app/views/check_login', view_func=views.check_login)

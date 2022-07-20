5f50c30db11e5a5d478b325c43cefccbb95d77dc
kelurLucky <Lucky@kelur.com>
2022-07-20 13:49:45 +0800

    recommendtion list

--- imovie_frontend/components/Detail/ScrollImage.js
+++ imovie_frontend/components/Detail/ScrollImage.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import SwiperImageComponent from "../Home/SwiperImage"
 import ScrollImageStyle from "./ScrollImage.less";
-const ScrollImage = ({list,title,more,isLogin,goHrefMore}) => {
+const ScrollImage = ({list,title,more,isLogin,goHrefMore,uid}) => {
     return (
       <React.Fragment>
         <style dangerouslySetInnerHTML={{ __html: ScrollImageStyle }} />
@@ -14,7 +14,7 @@ const ScrollImage = ({list,title,more,isLogin,goHrefMore}) => {
                   goHrefMore && goHrefMore();
               }}>More ></h6>}
             </div>
-            <SwiperImageComponent isLogin={isLogin} list={list}/>
+            <SwiperImageComponent uid={uid}  isLogin={isLogin} list={list}/>
         </div>
       </React.Fragment>
     )

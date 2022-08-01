dfdeca9e8ac04066168e1018bf0fab3b7a6e13c5
kelurLucky <Lucky@kelur.com>
2022-08-02 03:13:35 +0800

    Merge branch 'sprint2_lbp' into sprint2
    # Conflicts:
    #	iMovie_backend/.DS_Store
    #	iMovie_backend/app/login/__init__.py
    #	iMovie_backend/app/login/movies.py
    #	iMovie_backend/insert_movies.py
    #	iMovie_backend/movies_data.xlsx
    #	imovie_frontend/pages/movie/home.js

--- iMovie_backend/config.py
+++ iMovie_backend/config.py
@@ -14,3 +14,25 @@ SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
+
+# from urllib.parse import quote_plus as urlquote
+#
+#
+#
+#
+# SECRET_KEY = "123456789"
+# DIALECT = 'mysql'
+# DRIVER = 'pymysql'
+# USERNAME = 'root'
+# PASSWORD = '1234@qwer!'
+# HOST = 'rm-bp16q3qua05mfx407lo.mysql.rds.aliyuncs.com'
+# PORT = '3306'
+# DATABASE = 'imovie'
+#
+# # SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=UTF8MB4'.format(
+# #     DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE
+# # )
+#
+# SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USERNAME}:{urlquote(PASSWORD)}@{HOST}:{PORT}/{DATABASE}?charset=UTF8MB4'
+# SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+# SQLALCHEMY_TRACK_MODIFICATIONS = True

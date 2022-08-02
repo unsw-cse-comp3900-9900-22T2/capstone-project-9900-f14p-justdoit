bf08dcc4280fafa3b0a3b1fc9242059e7cbaefdd
z5278144@unsw.edu.au <z5278144@unsw.edu.au>
2022-08-03 03:16:40 +0800

    onlyshowML style update

--- imovie_frontend/pages/movie/onlyshowML.js
+++ imovie_frontend/pages/movie/onlyshowML.js
@@ -3,39 +3,14 @@ import OnlyshowMLStyle from "./onlyshowML.less";
 import React, {useEffect, useState} from 'react'
 import {Card, Input, message, Modal, Select} from "antd";
 import {ExclamationCircleOutlined, UserOutlined} from "@ant-design/icons";
-import {
-    addMoviesList,
-    addMovieToList,
-    delMovieFromList,
-    delMoviesList,
-    editMoviesList,
-    getMoviesInList,
-    getMoviesList,
-    getWatchlist,
-    searchResult
-} from "../MockData";
+import {getMoviesInList, getWatchlist} from "../MockData";
 import ImageDom from "../../components/Home/ImageDom";
 
-const {Meta} = Card;
-const {TextArea, Search} = Input;
-const {Option} = Select;
 const {confirm} = Modal;
-
 
 const OnlyShowML = ({USERMESSAGE}) => {
     const uid = USERMESSAGE  && USERMESSAGE.uid || null;
-    const [setTimeOutFun, changeSetTimeOut] = useState(null);
-    const [showModel, setShowModel] = useState(false);//添加movieList的弹窗
-    const [changeList, setChangeList] = useState(false);//切换movieList和详情列表
-    const [movieList, setMovieList] = useState([]);//影单列表
     const [movieListDetail, setMovieListDetail] = useState([]);//影单详情列表
-
-    const [listName, setListName] = useState("");//影单title
-    const [listDescription, setListDescription] = useState("");//影单描述
-    const [listNameFlag, setListNameFlag] = useState(true);//影单title的编辑切换
-    const [flag, setFlag] = useState(false);//是否显示电影的搜索列表
-    const [listDescriptionFlag, setListDescriptionFlag] = useState(true);//影单描述的编辑切换
-
     const [creatorInfo, setCreatorInfo] = useState('');//影单创建者
     const [movieListTitle, setMovieListTitle] = useState('');//影单title
     const [movieListDesc, setMovieListDesc] = useState('');//影单描述
@@ -47,8 +22,6 @@ const OnlyShowML = ({USERMESSAGE}) => {
         total: 0,
         sort: null,
     });
-    const [searchMovie, setSearchMovie] = useState([]);//
-    const [searchKeyWord, setSearchKeyWord] = useState('');//
 
     useEffect(() => {
         const pathname = window.location;
@@ -58,75 +31,6 @@ const OnlyShowML = ({USERMESSAGE}) => {
         // fetchData();
         getMoviesListDetails(molidParam[1])
     }, [])
-
-
-    /**影单列表的增删改查*/
-
-    //查询movieList列表
-    function fetchData() {
-        getMoviesList({uid}).then(res => {
-            if (res.code === 200) {
-                const {result} = res;
-                if (result) {
-                    console.log(result, '影单列表');
-                    setMovieList(result.result_list)
-                }
-            }
-        }).catch(err => {
-            console.log(err)
-        })
-    }
-
-    //添加一个moveList
-    const addMovieList = () => {
-        addMoviesList({
-            "uid": uid, "title": listName, "mid": '', "description": listDescription
-        }).then(res => {
-            if (res.code === 200) {
-                message.success(res.message);
-            }
-        }).catch(err => {
-            console.log(err)
-        }).finally(() => {
-            fetchData();
-            setShowModel(false)
-        })
-    }
-
-    //编辑movieList 需要传movieList的id
-    const editMovieList = (molid) => {
-        editMoviesList({
-            uid: uid, molid, title: listName, description: listDescription
-        }).then(res => {
-            if (res.code === 200) {
-                fetchData()
-                getMoviesListDetails(molid)
-            }
-        })
-    }
-
-    //删除movieList,需要传movieList的id
-    const deleteMovieList = (molid) => {
-        confirm({
-            title: 'Are you sure you want to clear this movielist?',
-            icon: <ExclamationCircleOutlined/>,
-            okText: "YES",
-            cancelText: "NO",
-            onOk() {
-                delMoviesList({uid, molid,}).then(res => {
-                    if (res.code === 200) {
-                        message.success("Clear successfully");
-                        fetchData();
-                    } else {
-                        message.error("Clear failed");
-                    }
-                }).finally(() => setChangeList(true))
-            }
-        });
-    }
-
-
-    /*影单详情列表的增删改查*/
 
     //查询影单详情列表
     const getMoviesListDetails = (molid) => {
@@ -151,42 +55,6 @@ const OnlyShowML = ({USERMESSAGE}) => {
             }
         })
     }
-
-    //添加电影到指定的影单
-    const addMovieListDetails = (mid) => {
-        addMovieToList({
-            uid,
-            molid,
-            mid,
-        }).then(res => {
-            if (res.code === 200) {
-                setFlag(false)
-                getMoviesListDetails(molid)
-                message.success('add success')
-            } else if (res.code === 400) {
-                message.error('add failed')
-            }
-        }).catch(err => {
-            console.log(err)
-            throw err
-        })
-    }
-
-    //从影单删除一个电影
-    const delMovieListDetails = (molid, mid) => {
-        delMovieFromList({
-            uid,
-            molid,
-            mid
-        }).then(res => {
-            console.log(res)
-        }).finally(() => {
-            fetchData()
-            setMovieListDetail([])
-            getMoviesListDetails(molid)
-        })
-    }
-
 
     function watchList(pageObj) {
         const _pageObj = pageObj || page;
@@ -216,128 +84,20 @@ const OnlyShowML = ({USERMESSAGE}) => {
         })
     }
 
-    //查询电影
-    const searchMovieList = (keyword) => {
-        searchResult({
-            page_index: page.number - 1,
-            page_size: page.size,
-            uid,
-            keyword
-        }).then(res => {
-            console.log(res)
-            if (res.code === 200) {
-                const {result} = res;
-                if (result) {
-                    setSearchMovie(result.movies)
-                }
-            }
-        })
-    }
-
     return <PageBase USERMESSAGE={USERMESSAGE} uid={USERMESSAGE.uid}>
-
         <style dangerouslySetInnerHTML={{__html: OnlyshowMLStyle}}/>
-        {
+        {<div className='Center'>
             <div className="watchListComponent">
                 <div className={"title-box"} style={{position: "relative", zIndex: 14}}>
                     <p className="title">
-                        <span style={{display: 'inline-block', marginRight: 10, color: '#1890ff'}}>{movieListTitle}&nbsp;&nbsp;&nbsp;&nbsp;
-                            <span style={{color: 'black'}}>Created by :&nbsp;&nbsp;<UserOutlined/>{creatorInfo.username}</span>
+                        <span style={{display: 'inline-block', marginRight: 10, color: '#1890ff'}}>{movieListTitle}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <span style={{color: 'black'}}>Created by :&nbsp;&nbsp;<UserOutlined/>&nbsp;{creatorInfo.username}</span>
                         </span>
                     </p>
 
-
-
-                    {/* <div className={"search-movie"}>
-                        <div className={"tag-search-logo"}>
-                            <SearchOutlined/>
-                        </div>
-                        <DocunceSelectComponent
-                            value={searchKeyWord || undefined}
-                            allowClear
-                            placeholder="Search Movie"
-                            size={'middle'}
-                            fetchOptions={async (keyword)=>{
-                                setSearchKeyWord(keyword);
-                                return searchBy({
-                                    uid : USERMESSAGE && USERMESSAGE.uid || null,
-                                    keyword
-                                }).then(res => {
-                                    if(res.code === 200){
-                                        const {result} = res;
-                                        if(result){
-                                            const {movies,count} = result;
-                                            const _length = count > 50? 50 : count;
-                                            const data = [];
-                                            for(let i = 0 ; i < _length ; i++){
-                                                data.push(movies[i]);
-                                            }
-                                            return {
-                                                list : data,
-                                                value : keyword
-                                            }
-                                        }else{
-                                            return {
-                                                list : [],
-                                                value : keyword
-                                            }
-                                        }
-                                    }else{
-                                        return {
-                                            list : [],
-                                            value : keyword
-                                        }
-                                    }
-                                }).catch(err => {
-                                    message.error("search error");
-                                    return {
-                                        list : [],
-                                        value : keyword
-                                    }
-                                })
-                            }}
-                            onChange={(newValue) => {
-                                setSearchKeyWord(newValue);
-                            }}
-                            style={{
-                                width: '90%',
-                            }}
-                            defaultActiveFirstOption={false}
-                            showArrow={false}
-                            filterOption={false}
-                            bordered={false}
-                            nodeDom={(options,inputValue)=>{
-                                return options &&
-                                    options.map((item) => {
-                                        if(!item){
-                                            return;
-                                        }
-                                        return (
-                                            <Option key={'labelData_' + item.mid + "_mid"} value={item.mid}>
-                                                <div
-                                                    style={{
-                                                        width: "100%",
-                                                        wordWrap: 'break-word',
-                                                        wordBreak: 'break-all',
-                                                        whiteSpace: 'normal',
-                                                    }}
-                                                    className={"label_data_user_movie_list_mid_search"}
-                                                    onClick={()=>{
-                                                        addMovieListDetails(item.mid)
-                                                        // window.location.href = "/movie/detail?movieId=" + item.mid;
-                                                    }}
-                                                >
-                                                    <h5>{item.moviename}{!!item.year && ("(" + item.year +")")}</h5>
-                                                </div>
-                                            </Option>
-                                        );
-                                    })
-                            }}
-                            showSearch />
-                    </div> */}
-
                 </div>
-                <span>{movieListDesc}</span>
+                <div className='des-box'><span className='des'>{movieListDesc}</span></div>
+                {/* <span>{movieListDesc}</span> */}
                 <div className={"imgBox"} style={{position: "relative", top: 50}}>
                     {movieListDetail.map((item, index) => <React.Fragment>
                         <ImageDom
@@ -370,9 +130,8 @@ const OnlyShowML = ({USERMESSAGE}) => {
                     </React.Fragment>)}
                 </div>
             </div>
+        </div>
         }
-
-
     </PageBase>
 }
 
